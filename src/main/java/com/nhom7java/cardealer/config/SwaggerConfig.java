2ad3058cda7ebdc24a4901d4550fc0015a0b6c46
Phuc Hoang <hoangmaithienphuc@gmail.com>
2023-04-10 14:33:29 +0700

    fix SwaggerConfig

--- src/main/java/com/nhom7java/cardealer/config/SwaggerConfig.java
+++ src/main/java/com/nhom7java/cardealer/config/SwaggerConfig.java
@@ -1,36 +1,28 @@
 package com.nhom7java.cardealer.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.mycompany.cardealer.controllers"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Car Dealer API")
-                .description("API documentation for Car Dealer Web Application")
-                .version("1.0")
-                .contact(new Contact("Your Name", "https://www.example.com", "your.email@example.com"))
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API")
+                        .version("1.0")
+                        .description("This is a sample API for demonstration purposes")
+                        .contact(new Contact()
+                                .name("John Doe")
+                                .email("john.doe@example.com")
+                                .url("https://example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
     }
 }
